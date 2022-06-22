8bbe273f3574a18a60dd87397b24c2bb97c659e5
Reynaldo R.L <rrodriguezl.1806@gmail.com>
2022-06-21 21:22:53 -0400

    add url to the server

--- src/utils/promise.js
+++ src/utils/promise.js
@@ -1,4 +1,5 @@
 const axios = require('axios')
-axios.defaults.baseURL = 'http://localhost:27001'
+// axios.defaults.baseURL = 'http://localhost:27001'
+axios.defaults.baseURL = 'https://codechallenge-node-senior-serv.herokuapp.com'
 
 module.exports = axios
